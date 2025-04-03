5e505dc98ef9a55e0e939056be0ae430dc5216db
Sterphane Roque <stermiss2@gmail.com>
2025-04-03 19:29:32 -0300

    fix: header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,11 +2,12 @@ import { CurrentAdmin } from './current-admin';
 
 export function Header() {
    return (
-      <header className='fixed h-[80px] w-screen px-[55px]'>
+      <header className='z-40 h-[80px] w-full px-8 md:px-[55px]'>
          <div className='flex h-full w-full items-center justify-between'>
             <img
                src='logo/logo.svg'
                alt='Logo'
+               className='w-32'
             />
 
             <div className='relative h-full w-[200px]'>
