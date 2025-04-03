e37d1bc503ade80187df117933732201bfcc8410
Sterphane Roque <stermiss2@gmail.com>
2025-04-03 19:29:13 -0300

    fix: correct pet accordion

--- src/components/pet-accordion.tsx
+++ src/components/pet-accordion.tsx
@@ -11,39 +11,46 @@ type PetAccordionProps = {
 
 export function PetAccordion({ pet }: PetAccordionProps) {
    const [isOpen, setIsOpen] = useState(false);
+
    return (
       <div
-         className='w-full max-w-[320px] text-white'
-         onClick={() => setIsOpen(!isOpen)}>
+         className='relative w-full text-white'
+         onClick={() => setIsOpen((prev) => !prev)}>
          <div
             className={cn(
                'hover:border-gradient flex h-24 w-full cursor-pointer items-center justify-center gap-[18px] rounded-[10px] bg-linear-to-tl from-gray-950 to-[#001E4D]',
                isOpen && 'border-gradient',
             )}>
-            <div className='flex gap-[18px]'>
-               <div className='flex h-16 w-16 items-center justify-center rounded-full bg-gradient-to-r from-cyan-400 to-blue-700'>
-                  <img
-                     src='icons/cat.svg'
-                     alt=''
-                  />
+            <div className='flex w-full justify-between gap-[18px] px-3.5 lg:px-6'>
+               <div>
+                  <div className='flex h-16 w-16 items-center justify-center rounded-full bg-gradient-to-r from-cyan-400 to-blue-700'>
+                     <img
+                        src='icons/cat.svg'
+                        alt=''
+                     />
+                  </div>
                </div>
-               <div className='flex w-[150px] flex-col justify-center'>
+               <div className='flex w-full flex-col justify-center'>
                   <div className='flex gap-2.5'>
                      <img
                         src='icons/collar.svg'
                         alt='collar'
+                        className='h-4 w-4'
                      />
-                     <span>{pet.name}</span>
+                     <span className='text-sm 2xl:text-base'>{pet.name}</span>
                   </div>
                   <div className='flex gap-2.5'>
                      <img
                         src='icons/user.svg'
                         alt=''
+                        className='h-4 w-4'
                      />
-                     <span>{pet.owner.name}</span>
+                     <span className='max-h-[20px] max-w-full min-w-[7.1875rem] truncate text-sm 2xl:text-base'>
+                        {pet.owner.name}
+                     </span>
                   </div>
                </div>
-               <div className='flex w-12 justify-center'>
+               <div className='flex w-10 justify-end'>
                   <img
                      src={`icons/${isOpen ? 'arrow-up' : 'arrow-down'}.svg`}
                      alt=''
@@ -60,8 +67,7 @@ export function PetAccordion({ pet }: PetAccordionProps) {
                   exit={{ opacity: 0, scale: 0.98 }}
                   transition={{ duration: 0.15, ease: 'easeOut' }}
                   className={cn(
-                     'mt-3.5 flex w-full flex-col justify-center gap-[18px] rounded-[10px] bg-linear-to-tl from-gray-950 to-[#001E4D] shadow-[0_0_30px_#0056E280]',
-                     isOpen && 'border-gradient',
+                     'border-gradient absolute z-10 mt-3.5 flex w-full flex-col justify-center gap-[18px] rounded-[10px] bg-linear-to-tl from-gray-950 to-[#001E4D] shadow-[0_0_30px_#0056E280]',
                   )}
                   onClick={(e) => e.stopPropagation()}>
                   <div className='mt-5 mr-[15px] ml-[23px] flex flex-col gap-2'>
