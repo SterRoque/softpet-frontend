d6fac3c7ab47858c2543d89b422e869c382be503
Sterphane Roque <stermiss2@gmail.com>
2025-04-10 02:46:46 -0300

    feat: update date formatting to use UTC methods and add new utility function to extract only numeric characters from a string

--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -1,7 +1,8 @@
 export const formatDate = (date: Date | string): string => {
-   const day = new Date(date).getDate().toString().padStart(2, '0');
-   const month = (new Date(date).getMonth() + 1).toString().padStart(2, '0');
-   const year = new Date(date).getFullYear();
+   const d = new Date(date);
+   const day = d.getUTCDate().toString().padStart(2, '0');
+   const month = (d.getUTCMonth() + 1).toString().padStart(2, '0');
+   const year = d.getUTCFullYear();
 
    return `(${day}/${month}/${year})`;
 };
