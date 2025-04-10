1e4719d6354274a4c0e65275fa53fbd4a9d93f57
Sterphane Roque <stermiss2@gmail.com>
2025-04-09 22:55:44 -0300

    feat: add formatDate utility function for consistent date formatting

---
+++ src/utils/format-date.ts
@@ -0,0 +1,7 @@
+export const formatDate = (date: Date | string): string => {
+   const day = new Date(date).getDate().toString().padStart(2, '0');
+   const month = (new Date(date).getMonth() + 1).toString().padStart(2, '0');
+   const year = new Date(date).getFullYear();
+
+   return `(${day}/${month}/${year})`;
+};
