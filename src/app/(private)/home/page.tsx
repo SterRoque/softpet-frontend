f886f49e920993883dbb3ccc7e9115d4b388f4e2
Sterphane Roque <stermiss2@gmail.com>
2025-04-10 22:28:56 -0300

    refactor: remove direct service call in Home component and simplify Pets rendering

--- src/app/(private)/home/page.tsx
+++ src/app/(private)/home/page.tsx
@@ -1,12 +1,5 @@
 import { Pets } from '@/components/pets';
-import { getPetsService } from '@/services/pets-service';
 
 export default async function Home() {
-   const { data: petsResponse } = await getPetsService({});
-
-   return (
-      <div>
-         <Pets pets={petsResponse.pets} />
-      </div>
-   );
+   return <Pets />;
 }
